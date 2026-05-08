cb209c95d36bda00ba5d2a317834727fa2f95585
Derickkitavi <113986568+k1tav1@users.noreply.github.com>
2026-05-08 08:33:21 +0300

    Add output for mnist.py classification report
    Added output for classification report from mnist.py.

---
+++ output for mint.py
@@ -0,0 +1,18 @@
+macbookpro@Kitavis-Macbook-Pro dataset01 % /usr/local/bin/python3 /Users/macbookpro/dataset01/mnist.py
+Classification report:
+              precision    recall  f1-score   support
+
+           0       1.00      0.98      0.99        53
+           1       0.96      1.00      0.98        53
+           2       1.00      0.98      0.99        53
+           3       0.96      0.89      0.92        53
+           4       0.98      0.95      0.96        57
+           5       0.95      0.98      0.96        56
+           6       0.98      0.98      0.98        54
+           7       1.00      1.00      1.00        54
+           8       0.91      0.98      0.94        52
+           9       0.96      0.96      0.96        55
+
+    accuracy                           0.97       540
+   macro avg       0.97      0.97      0.97       540
+weighted avg       0.97      0.97      0.97       540
